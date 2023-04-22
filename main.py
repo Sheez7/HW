593fa57580b1587c1d2ce6a18922f2b312cc5b40
Sheez7 <sheez2904@gmail.com>
2023-04-22 15:52:46 +0300

    4
    first commit

---
+++ main.py
@@ -0,0 +1,11 @@
+from application.salary import calculate_salary
+from application import get_employees
+import datetime
+
+def main():
+    calculate_salary()
+    get_employees()
+    datetime()
+
+if __name__ == '__main__':
+    main()
